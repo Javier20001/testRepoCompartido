07820e8e6664dbb8d9cbddd5ac9fd20f2c624a51
javier <kuznikjavier@gmail.com>
2023-10-13 15:30:00 -0300

    se agrego dni a la clase persona

--- ProyectoGitTest/src/main/java/test/Test.java
+++ ProyectoGitTest/src/main/java/test/Test.java
@@ -7,7 +7,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
-		Persona p1 = new Persona("javer","kuznik");
+		Persona p1 = new Persona("javer","kuznik",43197810);
 		System.out.println(p1.toString());
 	}
 
